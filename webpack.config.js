7bcbbb0f30c37a5f97b2a16eec30e520451b756c
philpatt <phillip.s.patterson1@gmail.com>
2018-03-29 15:48:10 -0700

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+
+    entry: {
+        app: './src/app.js'
+    },
+    output: {
+        filename: 'public/dist/bundle.js',
+        sourceMapFilename: 'public/dist/bundle.map'
+    },
+    devtool: '#source-map',
+    plugins: process.env.NODE_ENV === 'production' ? [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: true,
+                drop_console: true
+            }
+        })
+    ] : [],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+}
