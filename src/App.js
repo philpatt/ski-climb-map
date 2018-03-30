ce0171add1e94fd09247a760ab4a7d3e72653677
philpatt <phillip.s.patterson1@gmail.com>
2018-03-30 16:11:32 -0700

    google maps api loaded

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Map';
-
+import Places from './components/Places';
 class App extends Component {
 
   render() {
+    const location = {
+      lat: 40.7575285,
+      lng: -73.9884469
+    }
     return (
       <div>
         SKI CLIMB MAP
-        <Map />
+          <Map
+            zoom={ 10 }
+            center={ location }
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+        <Places />
+
       </div>
     );
   }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
 
 class Map extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            map: null
+        }
+    }
+    mapMoved(){
+        console.log('map moved', JSON.stringify(this.state.map.getCenter()))
+    }
+
+    mapLoaded(map){
+        if (this.state.map != null)
+            return 
+        
+        this.setState({
+            map: map
+        })
+    }
+    
     render(){
+    const markers = this.props.makers || []
+    console.log(this.props)
         return(
-            <div>
-                Map
-            </div>
+            <GoogleMap
+                ref={this.mapLoaded.bind(this)}
+                onDragEnd={this.mapMoved.bind(this)}
+                defaultZoom={this.props.zoom}
+                defaultCenter={ this.props.center }>
+                    {markers.map((marker, index)=>(
+                        <Marker {...marker} />
+                    )
+                )}
+            </GoogleMap>
         )
     }
 }
 
-export default Map
+export default withGoogleMap(Map)
